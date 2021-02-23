dd1f11a79ab1fb2d20814bed5d79acf98fd241ed
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-02-23 11:47:04 +0900

    initial release

---
+++ suparunidic/suparmodels/mkmodel.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+# pip3 install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+# pip3 install supar@git+https://github.com/yzhangcs/parser
+if [ $# -eq 0 ]
+then set `echo *-*`
+fi
+for M
+do if [ -s $M/$M.supar ]
+   then continue
+   elif [ -s ja_gsd_modern.conllu ]
+   then nawk '
+BEGIN{
+  f[0]="test.conllu";
+  f[1]="dev.conllu";
+  for(i=2;i<10;i++)
+    f[i]="train.conllu";
+}
+{
+  printf("%s\n",$0)>f[i%10];
+  if($0=="")
+    i++;
+}' ja_gsd_modern.conllu
+        case $M in
+        bert-base-japanese-*) B=cl-tohoku/$M ;;
+        distilbert-base-japanese) B=bandainamco-mirai/$M ;;
+        electra-small-japanese-*) B=Cinnamon/$M ;;
+        albert-japanese-v2) B=ALINEAR/$M ;;
+        *) B=$M ;;
+        esac
+        python3 -m supar.cmds.biaffine_dependency train -b -d 0 -p $M/$M.supar --epochs=1000 -f bert --bert $B --train train.conllu --dev dev.conllu --test test.conllu --embed=''
+   fi
+done
+exit 0
