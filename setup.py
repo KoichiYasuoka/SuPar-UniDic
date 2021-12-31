81496d8e8a46c65f6898ae88112a2b22c5548333
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-12-31 13:00:33 +0900

    version 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/SuPar-UniDic"
 
 setuptools.setup(
   name="suparunidic",
-  version="1.2.3",
+  version="1.2.4",
   description="Tokenizer POS-tagger Lemmatizer and Dependency-parser for modern and contemporary Japanese with BERT models",
   long_description=long_description,
   long_description_content_type="text/markdown",
